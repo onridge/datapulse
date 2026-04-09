780d53ba1b1387b580b32cd458fe88ce6cae6f83
Dima <onridge7311@gmail.com>
2026-04-09 21:44:14 +0200

    fix types

--- apps/api/src/types/context.ts
+++ apps/api/src/types/context.ts
@@ -1,8 +1,6 @@
-import type { Request, Response } from "express";
-
 export interface Context {
-  req: Request;
-  res: Response;
+  req: any;
+  res: any;
   userId: string | null;
   token: string | null;
 }
